c9a206ceefd602cd64bc0a08ae262d9a6c367638
yanqi.chen <yanqi.chen@duitang.com>
2017-03-12 12:49:18 +0800

    print student information

---
+++ 0002_SH_Chenyanqi.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# 第二次编程作业
+# 提示用户输入如下格式内容Jack Green, 21 ; Mike Mos, 9;后,分行打印学生信息,要求,姓名左对齐宽度20,年龄右对齐,宽度2,不足补零
+stu_info = raw_input('please enter students\' name and age :').split(';')
+index = 0
+for one in stu_info:
+    if one == '' or one.count(',') != 1:
+        break
+    one = one.split(',')
+    one[0] = one[0].strip()
+    one[1] = one[1].strip()
+    print '%-20s: %02d'%(one[0],int(one[1]))
+    index +=1
+
